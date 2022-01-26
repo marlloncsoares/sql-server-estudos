b02e384a174fcfd18eeb9da49ba5049e16b30803
marlloncsoares <marlloncsoares@gmail.com>
2022-01-26 01:44:48 -0300

    feat: adiciona select para obter o total de países

--- sql/select.sql
+++ sql/select.sql
@@ -10,4 +10,7 @@ SELECT * FROM dbo.PAIS WHERE ACTIVE = 1;
 -- Selectiona todos os dados inativos da tabela PAIS
 SELECT * FROM dbo.PAIS WHERE ACTIVE = 0;
 
+-- Seleciona a totalidade de dados da tabela PAIS
+SELECT COUNT(dbo.PAIS.ID) AS TOTAL_PAISES FROM dbo.PAIS
+
 GO
